79bfbfd190f490b3e8a0af523098a6c9536a6c43
xieshangwu <pal_xie@foxmail.com>
2017-07-13 18:18:50 +0800

    加入iconify

--- app/src/main/java/com/halove/fastec/EcApp.java
+++ app/src/main/java/com/halove/fastec/EcApp.java
@@ -3,6 +3,7 @@ package com.halove.fastec;
 import android.app.Application;
 
 import com.halove.latte.app.Latte;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 /**
  * Created by xieshangwu on 2017/7/13
@@ -12,6 +13,9 @@ public class EcApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Latte.init(this).withApiHost("http:192.168.1.105").configure();
+        Latte.init(this)
+                .withIcon(new FontAwesomeModule())
+                .withApiHost("http:192.168.1.105")
+                .configure();
     }
 }
--- latte-core/src/main/java/com/halove/latte/app/Configurator.java
+++ latte-core/src/main/java/com/halove/latte/app/Configurator.java
@@ -1,5 +1,9 @@
 package com.halove.latte.app;
 
+import com.joanzapata.iconify.IconFontDescriptor;
+import com.joanzapata.iconify.Iconify;
+
+import java.util.ArrayList;
 import java.util.WeakHashMap;
 
 /**
@@ -8,6 +12,7 @@ import java.util.WeakHashMap;
 
 public class Configurator {
     private static final WeakHashMap<String, Object> LATTE_CONFIGS = new WeakHashMap<>();
+    private static final ArrayList<IconFontDescriptor> ICONS = new ArrayList<>();
 
     private Configurator() {
         LATTE_CONFIGS.put(ConfigType.CONFIG_READY.name(), false);
@@ -26,7 +31,13 @@ public class Configurator {
     }
 
     public final void configure() {
+        initIcons();
         LATTE_CONFIGS.put(ConfigType.CONFIG_READY.name(), true);
+    }
+
+    public final Configurator withIcon(IconFontDescriptor descriptor) {
+        ICONS.add(descriptor);
+        return this;
     }
 
     public final Configurator withApiHost(String host) {
@@ -34,7 +45,16 @@ public class Configurator {
         return this;
     }
 
-    public void checkConfiguration() {
+    private void initIcons(){
+        if(ICONS.size() > 0) {
+            final Iconify.IconifyInitializer initializer = Iconify.with(ICONS.get(0));
+            for(int i=1;i<ICONS.size(); i++) {
+                initializer.with(ICONS.get(i));
+            }
+        }
+    }
+
+    private void checkConfiguration() {
         final boolean isReady = (boolean) LATTE_CONFIGS.get(ConfigType.CONFIG_READY.name());
         if(!isReady) {
             throw new RuntimeException("Configuration is not ready, call configure");
